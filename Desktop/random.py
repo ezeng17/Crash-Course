c9df74d9c380ddc80ddc870013b9b9981ede3a38
Elizabeth Zeng <Elizabeth@cmu-874057.wv.cc.cmu.edu>
2013-11-02 16:17:03 -0400

    random number generator

---
+++ Desktop/random.py
@@ -0,0 +1,15 @@
+#Second Version -- Will return some number from 0 to n-1 inclusive without restrictions
+
+import time
+
+def random(n):
+	x=time.clock()
+	while x<1:
+    		x=x*10
+	x=(x*12289)%1000
+	div=6151
+	while div<n:
+    		div=div*div-1
+	for i in range(n):
+   		x=(x*12289)%div
+	return int(x % n)
